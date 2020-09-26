6938c075d771e14499532347bf922df16c4f5ef8
AnthoniaOkafor <tonchuks@gmail.com>
2020-09-26 15:37:40 +0100

    completed templates and views for register, login and logout

--- src/projectapps/accounts/views.py
+++ src/projectapps/accounts/views.py
@@ -1,12 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+
 
 # Create your views here.
 
 def register (request):
-    return render (request, 'register.html')
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        email = request.POST['email']
+
+        if password1 == password2:
+            if User.objects.filter (username = username).exists():
+                print ('username taken')
+            elif User.objects.filter(email = email).exists():
+                print('email taken')
+            else:
+                user = User.objects.create_user(username = username, password=password1, email=email, first_name=first_name, last_name= last_name)
+                user.save()
+                print('user created')
+                return redirect('login')
+        else:
+            print('password not matching ...')
+            return redirect ('register')
+
+    else:
+        return render (request, 'register.html')
 
 def login (request):
-    pass
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password= password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            return redirect ('login')
+
+    else:
+        return render(request, 'login.html')
 
 def logout (request):
-    pass
+    auth.logout(request)
+    return redirect('login')
